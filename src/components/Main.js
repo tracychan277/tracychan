473c17cff85e8f1c1555e88f1369d35fd1bc0a77
Tracy Chan <ss.static@gmail.com>
2017-10-21 21:22:34 +1100

    Add 404 page to routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Site from './pages/Site';
 import Work from './pages/Work';
 import Contact from './pages/Contact';
+import NoRoute from './pages/NoRoute';
 
 const Main = () => (
 	<Router>
 		<div>
 			<nav>
-				<Link to="/about-me">About Me</Link>
-				<Link to="/this-site">This Site</Link>
-				<Link to="/my-work">My Work</Link>
-				<Link to="/contact">Contact</Link>
+				<ul>
+					<li><Link to="/about-me">About Me</Link></li>
+					<li><Link to="/this-site">This Site</Link></li>
+					<li><Link to="/my-work">My Work</Link></li>
+					<li><Link to="/contact">Contact</Link></li>
+				</ul>
 			</nav>
 			<main>
-				<Route exact path="/" component={Home} />
-				<Route path="/about-me" component={About} />
-				<Route path="/this-site" component={Site} />
-				<Route path="/my-work" component={Work} />
-				<Route path="/contact" component={Contact} />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/about-me" component={About} />
+					<Route path="/this-site" component={Site} />
+					<Route path="/my-work" component={Work} />
+					<Route path="/contact" component={Contact} />
+					<Route component={NoRoute}/>
+				</Switch>
 			</main>
 		</div>
 	</Router>
