6e9f85a49b6987906616522d2b1e81b67e74d14d
Tracy Chan <ss.static@gmail.com>
2017-10-29 23:56:28 +1100

    Add github link to the site source code

--- src/components/pages/Site.js
+++ src/components/pages/Site.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Site = () => (
 	<main>
 		<h1>This Site</h1>
+		<p>You can view the source code for this site at <a href="https://github.com/tracychan277/tracychan" rel="noopener noreferrer" target="_blank">GitHub</a>.</p>
 	</main>
 );
 
