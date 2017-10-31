610f5b12de156ae41f78d3068902502e4b1129d4
Tracy Chan <ss.static@gmail.com>
2017-10-31 14:42:00 +1100

    Revert "Remove gzip compression as this can be automated through CloudFront"
    This reverts commit 5c57b4c4f8d5b6f0a69a2bdb76393713aaeafc78.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -77,6 +78,13 @@ module.exports = {
 		new webpack.optimize.UglifyJsPlugin({
 			minimize: true,
 			sourceMap: true
+		}),
+		new CompressionPlugin({
+			asset: '[path].gz[query]',
+			algorithm: 'gzip',
+			test: /\.js$|\.css$/,
+			threshold: 10240,
+			minRatio: 0.8
 		})
 	]
 };
