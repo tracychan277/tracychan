edf2b004819b95ec73b112606c4b70f55786d300
Tracy Chan <ss.static@gmail.com>
2017-11-01 17:32:02 +1100

    Update About Me page with jumbotron and description

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const About = () => (
 	<main>
-		<h1>About Me</h1>
+		<div className="jumbotron">
+			<h1>About Me</h1>
+			<p>I was interested in web design and development pretty much as soon as I had access to the internet, and self-taught myself HTML and CSS from the age of 13.</p>
+		</div>
 	</main>
 );
 
