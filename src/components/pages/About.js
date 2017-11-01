a9da2d34a5313526c17f120fd0097d1393ac9d99
Tracy Chan <ss.static@gmail.com>
2017-11-01 19:10:20 +1100

    Added devicons to timeline

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DevIcon from '../Devicon';
 
 const About = () => (
 	<main>
@@ -16,6 +17,9 @@ const Timeline = () => (
 		<li>
 			<h3>2005</h3>
 			<p>3 years</p>
+			<DevIcon name="html5-plain"/>
+			<DevIcon name="css3-plain"/>
+			<DevIcon name="photoshop-plain"/>
 			<p>Amateur web design and development as a hobby. Self-taught basic HTML, CSS, JavaScript, and image creation/editing in Adobe Photoshop.</p>
 		</li>
 		<li>
@@ -25,26 +29,43 @@ const Timeline = () => (
 		</li>
 		<li>
 			<h3>2011</h3>
+			<DevIcon name="java-plain"/>
+			<DevIcon name="vim-plain"/>
 			<p>5 years</p>
 			<p>Formal programming and web development studies at university graduating with a Bachelor of Information Technology and Systems/Bachelor of Arts double degree.</p>
 		</li>
 		<li>
 			<h3>2015</h3>
 			<p>8 months</p>
+			<DevIcon name="oracle-original"/>
 			<p>Internship working in the government doing data fixes in Oracle SQL and proprietary SAS programming language.</p>
 		</li>
 		<li>
 			<h3>2015 - 2016</h3>
 			<p>8 months</p>
+			<DevIcon name="apache-plain"/>
+			<DevIcon name="mysql-plain"/>
+			<DevIcon name="bootstrap-plain"/>
 			<p>Small business client website project built in CakePHP and Bootstrap involving all phases of the SDLC from conception to delivery.</p>
 		</li>
 		<li>
 			<h3>2016</h3>
 			<p>1.5 years</p>
+			<DevIcon name="ubuntu-plain"/>
+			<DevIcon name="phpstorm-plain"/>
+			<DevIcon name="git-plain"/>
+			<DevIcon name="amazonwebservices-original"/>
+			<DevIcon name="nginx-original"/>
+			<DevIcon name="php-plain"/>
+			<DevIcon name="jquery-plain"/>
+			<DevIcon name="sass-original"/>
 			<p>Professional full-stack web-development experience working at an eCommerce web development agency specialising in Magento.</p>
 		</li>
 		<li>
 			<h3>2017 - present</h3>
+			<DevIcon name="webpack-plain"/>
+			<DevIcon name="babel-plain"/>
+			<DevIcon name="react-original"/>
 			<p>Exploring modern front-end development technologies and frameworks, in particular React, and seeking an exciting new role in front-end development.</p>
 		</li>
 	</ul>
