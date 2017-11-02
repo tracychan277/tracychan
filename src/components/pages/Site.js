8a49543db84da5c51fbb1767409a37c7fefbc7b2
Tracy Chan <ss.static@gmail.com>
2017-11-02 15:29:18 +1100

    Turn Site component into stateless functional component

--- src/components/pages/Site.js
+++ src/components/pages/Site.js
@@ -1,74 +1,63 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import {FaGithub} from 'react-icons/lib/fa';
 import Card from '../Card';
 import DevIcon from '../DevIcon';
 import DataList from '../DataList';
 
-export default class Site extends Component {
-	iconNames = [
-		'amazonwebservices-original',
-		'webpack-plain',
-		'babel-plain',
-		'react-original',
-		'sass-original',
-		'bootstrap-plain'
-	];
+const devtoolsList = {
+	'npm': 'Used to manage all JavaScript packages and dependencies.',
+	'webpack': 'Runs a development server on Express.js and automates building production-ready assets.',
+	'Babel': 'Used to transpile JSX into regular JavaScript and allow ES6+ syntax to be used in code.',
+	'ESLint': 'JavaScript linter for detecting code style issues.'
+};
 
-	devtoolsList = {
-		'npm': 'Used to manage all JavaScript packages and dependencies.',
-		'webpack': 'Runs a development server on Express.js and automates building production-ready assets.',
-		'Babel': 'Used to transpile JSX into regular JavaScript and allow ES6+ syntax to be used in code.',
-		'ESLint': 'JavaScript linter for detecting code style issues.'
-	};
+const frontendList = {
+	'React': 'Component-based JavaScript library for building reactive user interfaces.',
+	'React Router': 'Provides client-side routing functionality for single page applications in React.',
+	'Bootstrap': 'A mobile-first responsive frontend library that is dependent on jQuery and Popper.js.',
+	'React Icons': 'An interface for importing SVG icons from the most popular icon font libraries into React.',
+	'Devicon': 'Icon font for programming languages and development tools.'
+};
 
-	frontendList = {
-		'React': 'Component-based JavaScript library for building reactive user interfaces.',
-		'React Router': 'Provides client-side routing functionality for single page applications in React.',
-		'Bootstrap': 'A mobile-first responsive frontend library that is dependent on jQuery and Popper.js.',
-		'React Icons': 'An interface for importing SVG icons from the most popular icon font libraries into React.',
-		'Devicon': 'Icon font for programming languages and development tools.'
-	};
+const Site = () => (
+	<main>
+		<Jumbotron/>
+		<div className="row">
+			<Card heading="Development Tools">
+				<DataList list={devtoolsList}/>
+			</Card>
+			<Card heading="Frontend Libraries">
+				<DataList list={frontendList}/>
+			</Card>
+		</div>
+	</main>
+);
 
-	render() {
-		return (
-			<main>
-				<Jumbotron iconNames={this.iconNames}/>
-				<div className="row">
-					<Card heading="Development Tools">
-						<DataList list={this.devtoolsList}/>
-					</Card>
-					<Card heading="Frontend Libraries">
-						<DataList list={this.frontendList}/>
-					</Card>
-				</div>
-			</main>
-		);
-	}
-}
-
-const Jumbotron = (props) => (
+const Jumbotron = () => (
 	<div className="jumbotron">
 		<h1>This Site</h1>
-		<DevIcons iconNames={props.iconNames}/>
+		<DevIcons/>
 		<p>You can view the source code for this site at <a href="https://github.com/tracychan277/tracychan" rel="noopener noreferrer" target="_blank"><FaGithub/> GitHub</a>. This static site is hosted on an AWS S3 bucket and served through CloudFront.</p>
 	</div>
 );
 
-Jumbotron.propTypes = {
-	iconNames: PropTypes.array
-}
+const iconNames = [
+	'amazonwebservices-original',
+	'webpack-plain',
+	'babel-plain',
+	'react-original',
+	'sass-original',
+	'bootstrap-plain'
+];
 
-const DevIcons = (props) => (
+const DevIcons = () => (
 	<ul className="dev-icons">
 		{
-			props.iconNames.map((iconName) => {
+			iconNames.map((iconName) => {
 				return <li key={iconName}><DevIcon name={iconName}/></li>
 			})
 		}
 	</ul>
-)
+);
 
-DevIcons.propTypes = {
-	iconNames: PropTypes.array
-}
+export default Site;
