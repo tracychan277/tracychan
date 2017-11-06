3020164ed9449e0b2272742266f2d01d158e224a
Tracy Chan <ss.static@gmail.com>
2017-11-07 03:29:45 +1100

    Add input validation to contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {FaLinkedinSquare, FaEnvelope} from 'react-icons/lib/fa';
 
 const Contact = () => (
@@ -18,63 +18,144 @@ const Contact = () => (
 	</main>
 );
 
-const ContactForm = () => (
-	<form acceptCharset="UTF-8"
-		autoComplete="off"
-		encType="multipart/htmlForm-data"
-		method="post"
-		noValidate
-		action="https://tracychan277.wufoo.com/forms/z19hxi9t0im2hbm/"
-	>
-		<div className="form-group">
-			<div className="row">
-				<div className="col">
-					<input id="first-name"
-						name="Field1"
-						type="text"
-						className="form-control"
-						tabIndex="1"
-						placeholder="First Name"/>
-				</div>
-				<div className="col">
-					<input id="last-name"
-						name="Field2"
-						type="text"
-						className="form-control"
-						tabIndex="2"
-						placeholder="Surname"/>
-				</div>
-			</div>
-		</div>
-		<div className="form-group">
-			<div className="input-group">
-				<div className="input-group-addon">@</div>
-				<input id="email"
-					name="Field3"
-					type="email"
-					spellCheck="false"
-					className="form-control"
-					maxLength="255"
-					tabIndex="3"
-					placeholder="Email Address"/>
-			</div>
-		</div>
-		<div className="form-group">
-			<textarea id="message"
-				name="Field4"
-				className="form-control"
-				spellCheck="true"
-				rows="10" cols="50"
-				tabIndex="4"
-				placeholder="Type your message here..."
-			></textarea>
-		</div>
-		<input type="hidden"
-			id="idstamp"
-			name="idstamp"
-			value="5IO6jTuUwUmQEVQEAiRRRLpBHQb5zC/0KSlqgg3NL9k=" />
-        <input className="btn btn-primary" type="submit" value="Submit"/>
-	</form>
-);
+class ContactForm extends Component {
+	state = {
+		firstName: '',
+		lastName: '',
+		email: '',
+		message: '',
+		firstNameValid: false,
+		lastNameValid: false,
+		emailValid: false,
+		messageValid: false,
+		formValid: false
+	}
+
+	handleUserInput(e) {
+		const id = e.target.id;
+		const value = e.target.value;
+		this.setState(
+			{[id]: value},
+			() => {this.validateField(id, value)}
+		);
+	}
+
+	validateField(id, value) {
+		let firstNameValid = this.state.firstNameValid;
+		let lastNameValid = this.state.lastNameValid;
+		let messageValid = this.state.messageValid;
+		let emailValid = this.state.emailValid;
+		const notEmpty = value.trim().length >= 1;
+
+		switch(id) {
+			case 'firstName':
+				firstNameValid = notEmpty;
+				break;
+			case 'lastName':
+				lastNameValid = notEmpty;
+				break;
+			case 'message':
+				messageValid = notEmpty;
+				break;
+			case 'email':
+				emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+				break;
+		}
+
+		this.setState({
+			firstNameValid,
+			lastNameValid,
+			messageValid,
+			emailValid,
+		}, this.validateForm);
+	}
+
+	validateForm() {
+		this.setState({
+			formValid: this.state.firstNameValid &&
+				this.state.lastNameValid &&
+				this.state.messageValid &&
+				this.state.emailValid
+		});
+	}
+
+	errorClass(error) {
+		return (
+			error.length === 0 ? '' : 'has-error'	
+		);
+	}
+
+	render() {
+		return (
+				<form acceptCharset="UTF-8"
+					autoComplete="off"
+					encType="multipart/htmlForm-data"
+					method="post"
+					noValidate
+					action="https://tracychan277.wufoo.com/forms/z19hxi9t0im2hbm/"
+				>
+					<div className="form-group">
+						<div className="row">
+							<div className="col">
+								<input id="firstName"
+									name="Field1"
+									type="text"
+									className="form-control"
+									tabIndex="1"
+									placeholder="First Name"
+									value={this.state.firstName}
+									onChange={(event) => this.handleUserInput(event)}/>
+							</div>
+							<div className="col">
+								<input id="lastName"
+									name="Field2"
+									type="text"
+									className="form-control"
+									tabIndex="2"
+									placeholder="Surname"
+									value={this.state.lastName}
+									onChange={(event) => this.handleUserInput(event)}/>
+							</div>
+						</div>
+					</div>
+					<div className="form-group">
+						<div className="input-group">
+							<div className="input-group-addon">@</div>
+							<input id="email"
+								name="Field3"
+								type="email"
+								spellCheck="false"
+								className="form-control"
+								maxLength="255"
+								tabIndex="3"
+								placeholder="Email Address"
+								value={this.state.email}
+								onChange={(event) => this.handleUserInput(event)}/>
+						</div>
+					</div>
+					<div className="form-group">
+						<textarea id="message"
+							name="Field4"
+							className="form-control"
+							spellCheck="true"
+							rows="10" cols="50"
+							tabIndex="4"
+							placeholder="Type your message here..."
+							value={this.state.message}
+							onChange={(event) => this.handleUserInput(event)}
+						></textarea>
+					</div>
+					<input type="hidden"
+						id="idstamp"
+						name="idstamp"
+						value="5IO6jTuUwUmQEVQEAiRRRLpBHQb5zC/0KSlqgg3NL9k=" />
+					<input className="btn btn-primary"
+						type="submit"
+						value="Submit"
+						disabled={!this.state.formValid}/>
+				</form>
+		);
+	}
+}
 
 export default Contact;
