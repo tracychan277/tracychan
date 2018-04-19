1a7e654484a57f892e05d3d34e8c30fca6224ab8
Tracy Chan <ss.static@gmail.com>
2018-04-19 13:40:47 +1000

    Move logo image to the images folder

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
-import glasses from '../../glasses.png';
+import glasses from '../images/glasses.png';
 
 const Nav = () => (
 	<nav>
