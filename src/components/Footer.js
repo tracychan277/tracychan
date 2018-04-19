0bd55e202cbdc5688345a7ed80f43c3b76b419f8
Tracy Chan <ss.static@gmail.com>
2018-04-19 13:35:14 +1000

    Update footer year to 2018

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import {FaGithub} from 'react-icons/lib/fa';
 
 const Footer = () => (
 	<footer>
-		<p>&copy; Tracy Chan 2017 <a href="https://github.com/tracychan277/tracychan" title="View source code on Github" target="_blank" rel="noopener noreferrer"><FaGithub/></a></p>
+		<p>&copy; Tracy Chan 2018 <a href="https://github.com/tracychan277/tracychan" title="View source code on Github" target="_blank" rel="noopener noreferrer"><FaGithub/></a></p>
 	</footer>
 );
 
