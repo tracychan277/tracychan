03d4f82112f9497bdf1543d5305c2f11535d9b53
Tracy Chan <ss.static@gmail.com>
2019-06-01 21:10:06 +1000

    Set footer year to the current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {FaGithub} from 'react-icons/lib/fa';
 
+const year = new Date().getFullYear();
+
 const Footer = () => (
 	<footer>
-		<p>&copy; Tracy Chan 2018 <a href="https://github.com/tracychan277/tracychan" title="View source code on Github" target="_blank" rel="noopener noreferrer"><FaGithub/></a></p>
+		<p>&copy; Tracy Chan {year} <a href="https://github.com/tracychan277/tracychan" title="View source code on Github" target="_blank" rel="noopener noreferrer"><FaGithub/></a></p>
 	</footer>
 );
 
