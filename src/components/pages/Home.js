b1cb7648c6854e680a1b5f896e07f039466fe859
Tracy Chan <ss.static@gmail.com>
2019-06-01 21:22:15 +1000

    Remove line breaks in homepage content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,25 +9,18 @@ const Home = () => (
 	<main>
 		<div className="jumbotron">
 			<h1>Front-end web development</h1>
-			<p>I am a frontend web developer that is also comfortable working with the full stack. I am
-			passionate about delivering the best user web experience and getting the small details right.
-			<a href="/about-me">Click here</a> to read more about my professional development journey.</p>
+			<p>I am a frontend web developer that is also comfortable working with the full stack. I am passionate about delivering the best user web experience and getting the small details right. <a href="/about-me">Click here</a> to read more about my professional development journey.</p>
 		</div>
 		<div className="marketing-rows">
 			<MarketingRow heading="Responsive web design" image={ResponsiveSvg}>
 				<p>See your design come to life.</p>
-				<p>I specialise in converting a design into a fully functional website that performs
-				consistently across different devices and screen sizes.</p>
-				<p>Previous responsive redesign projects saw a 50% conversion rate on mobile for a previously
-				unresponsive desktop-centric website according to Google Analytics tracking.</p>
+				<p>I specialise in converting a design into a fully functional website that performs consistently across different devices and screen sizes.</p>
+				<p>Previous responsive redesign projects saw a 50% conversion rate on mobile for a previously unresponsive desktop-centric website according to Google Analytics tracking.</p>
 			</MarketingRow>
 			<hr/>
 			<MarketingRow heading="Modern front-end technologies and frameworks" image={CodeSvg}>
-				<p>As a perfectionist, I am constantly striving for self-improvement, and this includes
-				keeping up-to-date with the latest technologies and coding standards. I ensure that my code
-				is written to scale and loosely-coupled enough to be reusable.</p>
-				<p><a href="/this-site">Click here</a> to see what technologies and frameworks were used to
-				build this site.</p>
+				<p>As a perfectionist, I am constantly striving for self-improvement, and this includes keeping up-to-date with the latest technologies and coding standards. I ensure that my code is written to scale and loosely-coupled enough to be reusable.</p>
+				<p><a href="/this-site">Click here</a> to see what technologies and frameworks were used to build this site.</p>
 			</MarketingRow>
 			<hr/>
 			<MarketingRow heading="Cross-browser testing" image={CrossBrowserSvg}>
@@ -36,14 +29,12 @@ const Home = () => (
 				<DevIconList iconNames={['android-plain', 'apple-original', 'ubuntu-plain', 'windows8-original']}/>
 			</MarketingRow>
 		</div>
-		<p className="text-muted"><small>Vector graphics designed by
-		<a href="https://www.flaticon.com/authors/freepik" target="_blank" rel="noopener noreferrer">
-			Freepik
-		</a> at Flaticon.</small></p>
+		<p className="text-muted">
+			<small>Vector graphics designed by <a href="https://www.flaticon.com/authors/freepik" target="_blank" rel="noopener noreferrer">Freepik</a> at Flaticon.</small>
+		</p>
 		<div className="jumbotron">
 			<h3>Like what you see?</h3>
-			<p>I am not currently open to job offers, but feel free to reach out to me for networking
-			purposes.</p>
+			<p>I am not currently open to job offers, but feel free to reach out to me for networking purposes.</p>
 			<a className="btn btn-success btn-lg" href="/contact">Get in touch</a>
 		</div>
 	</main>
